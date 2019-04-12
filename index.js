64c11df7f571244dbb7634627cdfbaf72cd4ff9c
lbeckman314 <lbeckman314@gmail.com>
2019-04-12 11:55:25 -0700

    Adding postgresql.

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
