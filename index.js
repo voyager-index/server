abc89a768fc492809f4de4df90ea41d7c95a6f21
lbeckman314 <lbeckman314@gmail.com>
2019-04-15 13:25:04 -0700

    Added comments to index.js.

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+// Listen on port 5000, or use "export PORT=31415"
+// to start on port 31415
 const PORT = process.env.PORT || 5000
-const express = require('express')
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+
 const fetch = require("node-fetch");
 const https = require('https');
 const path = require('path')
-const { Pool } = require('pg');
 
+// POST requests
+const bodyParser = require('body-parser');
+
+// Use ejs for templating.
+const ejs = require('ejs');
+
+// Used to connect to PostgreSQL database.
+const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
+// Use express for the web server.
+const express = require('express')
 const app = express();
-app.use(bodyParser());
-app.set('view engine', 'ejs');
 
+// ---------- //
+// Pages
+// ---------- //
 
+// database page
 app.get('/db', async (req, res) => {
     try {
         const client = await pool.connect()
@@ -27,21 +38,26 @@ app.get('/db', async (req, res) => {
         console.error(err);
         res.send("Error " + err);
     }
-})
+});
 
 
+// city page
 app.get('/city', async(req, res) => {
     const city = "portland";
     const city_req = await getCity(city);
     const city_name = city_req.name;
     const city_image = city_req.image;
     res.render('pages/city',{city_name:city_name, city_image:city_image});
-})
+});
 
 
+// city page
 app.post('/city', async (req, res) => {
-    var qParams = [];
+
+    // get city name from POST body.
     const city = encodeURI(req.body.city);
+
+    // error page
     const err = () => {
         res.render('pages/city',{city_name:"City not found.",city_image:"https://www.rust-lang.org/logos/error.png"});
     }
@@ -55,17 +71,20 @@ app.post('/city', async (req, res) => {
     catch(error) {
         err();
     }
-})
+});
+
+// Basic post request that receives bounding box, returns city points
+app.post('/bounding', (req, res) => {
+    const bounding_box = encodeURI(req.body.bounding_box);
+});
 
 
-app
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening at http://localhost:${ PORT }`))
+// -------------------- //
+// Helper functions
+// -------------------- //
 
-
+// Used to interprete JSON objects returned by requests to API's.
+// Fetches "url" and returns whatever value is associated with the "object".
 async function getThing(url, object) {
     return fetch(url)
     .then(response => response.json())
@@ -75,6 +94,7 @@ async function getThing(url, object) {
 }
 
 
+// Returns a city's name and image.
 async function getCity(city) {
     const city_search = "https://api.teleport.org/api/cities/?search=" + city;
     const city_url = 'data._embedded["city:search-results"][0]._links["city:item"].href';
@@ -95,3 +115,13 @@ async function getCity(city) {
 
     return ret;
 }
+
+
+// Start app.
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .use(bodyParser())
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening at http://localhost:${ PORT }`))
