e135bd28ff46db8642581ffeb241f031f53f16c5
lbeckman314 <lbeckman314@gmail.com>
2019-05-15 18:34:21 -0700

    Add button groups to filter.

--- public/filter.js
+++ public/filter.js
@@ -16,6 +16,38 @@ $(".pollution").click(() => {
     changeMarkers();
 });
 
+// mimics radio button for filters
+function button_group(...args) {
+    args.forEach((arg) => {
+        $('.' + arg).click((e) => {
+
+            // remove active class from other buttons
+            args.forEach((a) => {
+                if (a != arg) {
+                    $('.' + a).removeClass('active');
+                }
+            });
+
+            // change markers
+            changeMarkers();
+        });
+    });
+}
+
+// city size filter
+button_group('rural', 'town', 'city', 'metro');
+
+// temperature filter
+button_group('cold', 'temperate', 'warm', 'hot');
+
+// Gets list of filters from Voyager module (/public/main.js).
+// Gets bouding box from Voyager module (/public/main.js).
+// Sends both to /bounding as POST request.
+
+// /index.js receives request, queries database, ranks cities,
+// and returns city array (as data.cities).
+
+// Calls Voayger.buildFeatures() with city array.
 function changeMarkers() {
     const filters = Voyager.getFilters();
 
