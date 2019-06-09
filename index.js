ec131cc679460c2c5233e3bf544c422a7f005858
lbeckman314 <lbeckman314@gmail.com>
2019-06-08 23:05:25 -0700

    Add json data file.

--- index.js
+++ index.js
@@ -29,10 +29,18 @@ const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 
+const fs = require("fs");
+
 const DEBUG = false;
 
 // number of elements on the grid page
 const grid_number = 16;
+
+let cities = [];
+fs.readFile("voyager-index-data.json", function(err, buffer) {
+    //console.log(JSON.parse(buffer));
+    cities = JSON.parse(buffer);
+});
 
 // common database string used in all city queries.
 const common = `
@@ -184,13 +192,13 @@ app.all('/city-search', async (req, res) => {
 
     let query = ``;
     if (search.id != null) {
-       query = `
+        query = `
             ${common}
             WHERE C.id = ${search.id}
         ;`;
     }
     else {
-       query = `
+        query = `
             ${common}
             WHERE C.name ILIKE '%${search.city}%'
             ORDER BY P.total DESC
@@ -198,7 +206,7 @@ app.all('/city-search', async (req, res) => {
         ;`;
     }
 
-   //console.log(query);
+    //console.log(query);
 
     const action = (results) => {
         const filters = [];
@@ -294,18 +302,9 @@ app.post('/bounding', async (req, res) => {
 
     var query = `
         ${common}
-
-        WHERE
-        (C.lon >= ${bottom_left_lon} OR
-        C.lon >= ${lon_wrap}) AND
-        C.lat >= ${bottom_left_lat} AND
-
-        (C.lon <= ${lon_wrap_neg} OR
-        C.lon <= ${top_right_lon}) AND
-        C.lat <= ${top_right_lat}
     `;
 
-   for (var i = 0; i < filters.length; i++){
+    for (var i = 0; i < filters.length; i++){
         if(filters[i] == "internet"){
             query += ' AND I.Speed > 1';
         }
@@ -336,28 +335,66 @@ app.post('/bounding', async (req, res) => {
         if(filters[i] == "intlairports"){
             query += ' AND (ia.Exists = true)'
         }
-   }
+    }
 
     query += " ORDER BY P.total DESC LIMIT 100;";
     //console.log(query);
 
     const action = (results) => {
-        var cityRank = rankCities(results, filters);
+        const cityRank = rankCities(results, filters);
         res.send(cityRank);
-        //console.log(cityRank);
+
+        console.log(results);
+        fs.writeFile("rows.json", JSON.stringify(results), (err) => {
+            if (err) console.log(err);
+            console.log("Successfully Written to File.");
+        });
+    }
+
+    const std = `
+        cities[i].lon >= bottom_left_lon
+        && cities[i].lat >= bottom_left_lat
+        && cities[i].lon <= top_right_lon
+        && cities[i].lat <= top_right_lat
+    `
+
+    const wrap = `
+        (cities[i].lon >= bottom_left_lon
+            || cities[i].lon >= lon_wrap)
+        && cities[i].lat >= bottom_left_lat
+        && (cities[i].lon <= top_right_lon
+            || cities[i].lon <= lon_wrap_neg)
+        && cities[i].lat <= top_right_lat
+    `
+
+    let lat_lon = ``;
+    if (bottom_left_lon > top_right_lon) {
+        lat_lon = wrap
+    }
+    else {
+        //lat_lon = std;
+        lat_lon = wrap;
+    }
+
+    let num = 0;
+    let results = [];
+    for (let i = 0; i < cities.length; i++) {
+        if (eval(lat_lon)) {
+            results.push(cities[i]);
+            num += 1;
+        }
+        if (num >= 100) {
+            break;
+        }
     }
 
     try {
-        const t0 = process.hrtime();
-        const results = await swimming_pool(query, action);
-        const t1 = process.hrtime(t0);
-        console.info('Execution time (hr): %ds %dms', t1[0], t1[1] / 1000000)
+        const cityRank = rankCities(results, filters);
+        res.send(cityRank);
     } catch (err) {
         console.error(err);
         res.send('Error:', err);
     }
-
-
 });
 
 
@@ -467,10 +504,10 @@ app.post('/issues-submit', (req, res) => {
         },
         body: post_data,
     })
-    .then(response => response.json())
-    .catch(err => console.error('Error:', err))
-    .then(response => console.log('Success:', response))
-    .then(res.render("pages/issues-submit", {issue_title: issue_title, issue_body: issue_body, issue_type: issue_type}));
+        .then(response => response.json())
+        .catch(err => console.error('Error:', err))
+        .then(response => console.log('Success:', response))
+        .then(res.render("pages/issues-submit", {issue_title: issue_title, issue_body: issue_body, issue_type: issue_type}));
 });
 
 // Start app.
@@ -493,12 +530,298 @@ If there are more than one, then the score is a combination of the values of the
 plus the rest of the factors, using our values, weighted down to be less impactful.
 */
 function rankCities(cities, filters){
-        // Adjust to only 1 decimal place
-        var roundedRank = 10;
+    //True False values don't matter, because they are filtered out.
+    var selectedMonth;
+    // Month included?
+    const group = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+    var includeMonth = false;
+    for (var j = 0; j < 12; j++){
+        if (filters.includes(group[j])){
+            includeMonth = true;
+            selectedMonth = group[j];
+        }
+    }
+    let includeNonRank = false;
+    let nonRankCount = 0;
+    if(filters.includes("palms") || filters.includes("beaches") || filters.includes("airports") || filters.includes("intlairports") ){
+        includeNonRank = true;
+        for (var x = 0; x < filters.length; x++){
+            if(filters[x] == "palms" || filters[x] == "beaches" || filters[x] == "airports" || filters[x] == "intlairports"){
+                nonRankCount += 1;
+            }
+        }
+    }
+
+    // Population
+    let includePop = false;
+    if(filters.includes('rural') || filters.includes('town') || filters.includes('city') || filters.includes('metro')){
+        includePop = true;
+    }
 
     var i;
     var rankedCities = [];
     for (i = 0 ; i < cities.length; i++){
+        var filterrank = 0; // Starting rank for each city, for filter button selections
+        var weightedrank = 0; // This ranking is for information we have that the user did not select to filter by
+        var weight = .2; // Weight given to each section depending on whether or not it was selected as filter
+        var weightedCount = 0; //Count of items that may be given a weighted score.
+        const pop = Number(cities[i]["population"]);
+
+        // Rank based on filter buttons. These names are in the class list for each button on index.ejs
+
+        //Internet Speed
+        var mbps = cities[i]["mbps"];
+        var internetRank = 0;
+        if (mbps != null){
+            if (mbps > 20){
+                internetRank += 10;
+            } else {
+                internetRank += mbps/2;
+            }
+        }
+        if(filters.includes("internet")){  // MBPS is greater than 1
+            filterrank += internetRank;
+        } else {
+            weightedrank += internetRank;
+            weightedCount++;
+        }
+
+        // Pollution
+        var pollution = Number(cities[i]["pollution"]) || 0;
+        if (pollution > 100){
+            pollution = 100;
+        }
+        var pollutionRank = (100 - pollution)/10;
+        if(filters.includes("pollution")){ // Index is null or less than 100
+            filterrank += pollutionRank;
+        } else {
+            weightedrank += pollutionRank;
+            weightedCount++;
+        }
+
+        // Weather
+        var avgUV;     // Integer, index of 0 - 16, * 16       For higher accuracy, so its 0 through 256
+        var avgPrecip; // Integer, mm
+        var avgTemp; // Integer, degrees C * 10
+        var uvRank, precipRank, tempRank;
+
+        // Get weather without month
+        if (includeMonth == false){
+            // get averages
+            avgUV = getAvgThing(cities[i], 'uv');
+            avgPrecip = getAvgThing(cities[i], 'precip');
+            avgTemp = getAvgThing(cities[i], 'temp');
+
+
+        } else { // Get weather with month filter included
+            // get averages
+            avgUV = getMonthUV(cities[i], selectedMonth);
+            avgPrecip = getMonthPrecip(cities[i], selectedMonth);
+            avgTemp = getMonthTemp(cities[i], selectedMonth);
+        }
+
+        /*
+RANKING DONE BELOW
+*/
+
+        // UV Ranking
+        if(avgUV > 160){ // Over UV idx of 10
+            uvRank = 2.56 - avgUV/100;
+        } else {
+            uvRank = Math.min(12.56 - avgUV/16, 10); //uv index of 10 gives 2.6 to ranking, uv index of 0 gives 10 to ranking
+        }
+        uvRank = Math.round(uvRank); //No areas have true 0 uv exposure, so this is just to round up.
+        if(filters.includes('uv')){
+            filterrank += uvRank;
+        } else {
+            weightedrank += uvRank;
+            weightedCount++;
+        }
+
+        //Precip ranking
+        if (avgPrecip <= 150){
+            precipRank = (200 - avgPrecip)/20; // 0 adds 10 to rank, 150 adds 2.5
+        } else if (rank < 300) {
+            precipRank = 2;
+        } else { // At a certain point it doesnt matter, you're just in the rain.
+            precipRank = 1;
+        }
+        if (filters.includes('precipitation')){ // in MM, from 0 to ~800
+            filterrank += precipRank;
+        } else {
+            weightedrank += precipRank;
+            weightedCount++;
+        }
+
+        // Temp ranking
+        if (filters.includes('cold') ){
+            // 0 f to 45 f == -17.7 C to 7.2
+            if (avgTemp >= -177 && avgTemp <= 72){
+                tempRank = 10;
+            } else if (avgTemp < -177) {
+                tempRank = Math.max((20 + (avgTemp + 177))/2, 0);
+            } else {
+                tempRank = Math.max((20 - (avgTemp - 72))/2, 0);
+            }
+        } else if ( filters.includes('temperate')){
+            // 45 f to 72 f ==  7.2 C to 22.2 C
+            if (avgTemp >= 72 && avgTemp <= 222){
+                tempRank = 10;
+            } else if ( avgTemp < 72 && avgTemp > 0 ) {
+                tempRank = Math.max((20 - (72- avgTemp))/2, 0);
+            } else if ( avgTemp > 222){
+                tempRank = Math.max((20- (avgTemp - 222))/2, 0);
+            } else {
+                tempRank = 0;
+            }
+
+        } else if (filters.includes('warm')){
+            // 75 f to 90 f == 23.8 C to 32.2
+            if (avgTemp >= 238 && avgTemp <= 322){
+                tempRank = 10;
+            } else if ( avgTemp < 238 && avgTemp > 0 ) {
+                tempRank = Math.max((20 - (238 - avgTemp))/2, 0);
+            } else if ( avgTemp > 322){
+                tempRank = Math.max((20- (avgTemp - 322))/2, 0);
+            } else {
+                tempRank = 0;
+            }
+        } else if (filters.includes('hot')){
+            // 85 f to 101 f == 29.4 C to 38.3
+            if (avgTemp >= 294 && avgTemp <= 383){
+                tempRank = 10;
+            }
+            else if ( avgTemp < 294 && avgTemp > 0 ) {
+                tempRank = Math.max((20 - (294 - avgTemp))/2, 0);
+            } else if ( avgTemp > 383){
+                tempRank = Math.max((20- (avgTemp - 383))/2, 0);
+            } else {
+                tempRank = 0;
+            }
+        } 
+        if (filters.includes('cold') || filters.includes('temperate') || filters.includes('warm') || filters.includes('hot')){
+            filterrank += tempRank;
+        }
+
+        // Purchasing power
+        // data values between 1.29 and .14
+
+        var purchasePower = cities[i]["purchasingpower"];
+        var pppRank;
+        if(filters.includes('purchase')){
+            if (purchasePower >= 1){
+                pppRank = 5 - (purchasePower -1)*10;
+            }
+            else {
+                pppRank = 5 + ((1 - purchasePower)*10)/2;
+                if(pppRank > 10){
+                    pppRank = 10;
+                } 
+            }
+            filterrank += pppRank;
+        }
+
+        // Socioeconomic filter
+        var povertyindex = cities[i].povertyindex;
+        //console.log("Socioeconomic filter:", povertyindex);
+        if (filters.includes('high-poverty-index')){
+            filterrank += povertyindex/10; //100% severe povery will give a 10, and no poverty gives a 0
+        }
+        else if (filters.includes('medium-poverty-index')){
+            if(povertyindex == 0){
+                filterrank += 3;
+            }
+            else if(povertyindex < 15){
+                filterrank += 8;
+            }
+            else{
+                filterrank += 0;
+            }
+        }
+        else if (filters.includes('low-poverty-index')){
+            if(povertyindex == 0){
+                filterrank += 10;
+            }
+            else if(povertyindex < 5){
+                filterrank += 5;
+            }
+            else{
+                filterrank += 0;
+            }
+        }
+        else {
+            weightedrank += 4 - povertyindex;
+            weightedCount++;
+        }
+
+        // Homicides and female homicides by country
+        // Max is 83, avg is 7.5, America is 4.9, 0s means no data
+        var totalHoms = Number(cities[i]["totalhomicides"]) || 0;
+        var femaleHoms = Number(cities[i]["femalehomicides"]) || 0;
+        var safe, safeforwomen;
+        if(totalHoms > 8 || totalHoms == 0){
+            safe = 0;
+        } else {
+            safe = 10 - totalHoms;
+        }
+
+        if(femaleHoms > 8 || femaleHoms == 0){
+            safeforwomen = 0;
+        } else {
+            safeforwomen = 10 - femaleHoms;
+        }
+
+        if (filters.includes('safe')){
+            filterrank += safe;
+        } else {
+            weightedrank += safe;
+            weightedCount++;
+        }
+
+        if (filters.includes('safeforwomen')){
+            filterrank += safeforwomen;
+        } else {
+            weightedrank += safeforwomen;
+            weightedCount++;
+        }
+
+        var rank;
+
+        if(filters.length == 0){
+            rank = weightedrank/weightedCount;
+        }
+        else if  (filters.length == 1 && (includeMonth || includePop || includeNonRank) ){
+            rank = weightedrank/weightedCount;
+        }
+        else if (filters.length == 1){
+            rank = filterrank;
+        }
+        else if(filters.length == 2){
+            if( !(includeMonth || includePop || includeNonRank) ) {
+                rank = (filterrank + (weightedrank * weight) ) / (filters.length + (weightedCount * weight));
+            }
+            else if ((includeMonth && !(includePop || includeNonRank)) || (includePop && !(includeMonth || includeNonRank)) || (includeNonRank && !(includePop || includeMonth))){
+                if(nonRankCount == 2){
+                    rank = weightedrank/weightedCount;
+                }
+                else{
+                    rank = filterrank;
+                }
+            }
+            else{
+                rank = weightedrank/weightedCount;
+            }
+        }
+        else if (filters.length == 3 && includeMonth && includePop && includeNonRank){
+            rank = weightedrank/weightedCount;
+        }
+        else {
+            rank = (filterrank + (weightedrank * weight) ) / (filters.length + (weightedCount * weight));
+        }
+
+        // Adjust to only 1 decimal place
+        var roundedRank = Math.round(rank * 10)/10;
+
         //name, lon, lat, rank, id
         if (cities[i]['image']) {
             rankedCities.push({
@@ -519,8 +842,8 @@ function rankCities(cities, filters){
                 "id": cities[i]["id"]
             });
         }
-    }
 
+    }
 
     var returnVal = {'cities': rankedCities};
     return returnVal;
@@ -534,7 +857,6 @@ async function swimming_pool(query = '', action) {
         const result = await client.query(query);
         const results = result ? result.rows : null;
         client.release();
-        console.log(results);
 
         // return results
         if (arguments.length == 1) {
@@ -545,7 +867,7 @@ async function swimming_pool(query = '', action) {
             action(results);
         }
 
-    //TODO: It should probably just show the data that it can get, or say that it can't find data.
+        //TODO: It should probably just show the data that it can get, or say that it can't find data.
     } catch (err) {
         console.error(err);
     }
