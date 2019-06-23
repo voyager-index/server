84d55ed1dbd7b13c634b9255888a8e054ae2cf3b
lbeckman314 <lbeckman314@gmail.com>
2019-06-22 23:26:04 -0700

    Add checked to theme radio buttons.
    - Fix demo jump on ping/pong.

--- public/demo.js
+++ public/demo.js
@@ -5,48 +5,53 @@ import Autolinker from 'autolinker';
 import $ from 'jquery';
 import jQuery from 'jquery';
 const AU = require('ansi_up');
-const ansi_up = new AU.default;
+const ansi_up = new AU.default();
 
 const production_url = 'wss://liambeckman.com:8181';
 const development_url = 'ws://localhost:8181';
 const DEV = false;
 
 // namespace
-var MYLIBRARY = MYLIBRARY || (function() {
-    var _args = {}; // private
+var MYLIBRARY =
+    MYLIBRARY ||
+    (function() {
+        var _args = {}; // private
 
-    return {
-        init : (Args) => {
-            _args = Args;
-            // some other initialising
-        },
-        helloWorld : () => {
-            return _args[0];
-        }
-    };
-}());
+        return {
+            init: Args => {
+                _args = Args;
+                // some other initialising
+            },
+            helloWorld: () => {
+                return _args[0];
+            },
+        };
+    })();
 
-export {MYLIBRARY};
+export { MYLIBRARY };
 
 $(document).ready(() => {
-    let terminals = $(".terminals");
+    let terminals = $('.terminals');
 
-    let duplicateTerminal = $("#duplicate-terminal");
+    let duplicateTerminal = $('#duplicate-terminal');
     if (duplicateTerminal) {
         duplicateTerminal.onclick = () => {
             if (terminals.length < 2) {
                 dup();
             }
-        }
+        };
     }
 
-    let examples = $(".demo-examples");
+    let examples = $('.demo-examples');
     for (let i = 0; i < examples.length; i++) {
         let example = examples[i].innerHTML;
         examples[i].onclick = () => {
-            terminals[0].innerHTML = terminals[0].innerHTML.replace(/.*$/ ,"> " + example);
+            terminals[0].innerHTML = terminals[0].innerHTML.replace(
+                /.*$/,
+                '> ' + example
+            );
             terminals[0].focus();
-        }
+        };
     }
 
     let socket = getSocket();
@@ -56,11 +61,9 @@ $(document).ready(() => {
         if (socket.isAlive === false) {
             socket = getSocket();
             doTerminal(terminals[0], socket);
-        }
-
-        else {
+        } else {
             socket.isAlive = false;
-            socket.send("ping");
+            socket.send('ping');
         }
     }, 3000);
 });
@@ -71,8 +74,7 @@ function getSocket() {
         // development
         let socket = new WebSocket(development_url);
         return socket;
-    }
-    else {
+    } else {
         // production
         let socket = new WebSocket(production_url);
         return socket;
@@ -81,12 +83,12 @@ function getSocket() {
 
 // adds an additional terminal below the first.
 function dup() {
-    let terminals = $(".terminals");
-    let terminalContainer = $(".terminal");
-    let buttonContainer = $("#button-container");
+    let terminals = $('.terminals');
+    let terminalContainer = $('.terminal');
+    let buttonContainer = $('#button-container');
 
     let clone = $('<textarea>');
-    clone.className = "terminals";
+    clone.className = 'terminals';
     terminalContainer.appendChild(clone);
 
     // Create WebSocket connection.
@@ -95,30 +97,31 @@ function dup() {
     doTerminal(clone, socket);
 
     let removeTerminal = $('span');
-    removeTerminal.id = "remove-terminal";
-    removeTerminal.innerHTML = "-";
+    removeTerminal.id = 'remove-terminal';
+    removeTerminal.innerHTML = '-';
     buttonContainer.appendChild(removeTerminal);
 
     removeTerminal.onclick = () => {
         clone.remove();
         removeTerminal.remove();
         socket.close();
-    }
+    };
 }
 
 // used for the zigzag tcp chat program
 function zigzagPort(message) {
-    let terminals = $(".terminals");
+    let terminals = $('.terminals');
     let original = terminals[0];
     let clone = terminals[1];
 
-    if (message.includes("port number:")) {
-
-        let port = message.split(" ");
+    if (message.includes('port number:')) {
+        let port = message.split(' ');
         port = port[port.length - 1];
 
-        terminals[1].innerHTML = terminals[1].innerHTML.replace(/.*$/ ,"> " + "zigzag-client " + parseInt(port));
-
+        terminals[1].innerHTML = terminals[1].innerHTML.replace(
+            /.*$/,
+            '> ' + 'zigzag-client ' + parseInt(port)
+        );
     }
 }
 
@@ -141,57 +144,60 @@ function doTerminal(terminal, socket) {
     }
 
     terminal.spellcheck = false;
-    console.log("Connecting to server...");
+    console.log('Connecting to server...');
 
-    let info = $("#info")[0];
-    info.innerHTML = "Status: Connecting...";
-    info.style.backgroundColor = "#ff357a";
+    let info = $('#info')[0];
+    info.innerHTML = 'Status: Connecting...';
+    info.style.backgroundColor = '#ff357a';
 
     // Connection opened
-    socket.onopen = (event) => {
-        console.log("Sending initial message to server.");
-        info.innerHTML = "Status: Connected. Press ENTER to blast off!";
-        info.style.backgroundColor = "#49ccd4";
+    socket.onopen = event => {
+        console.log('Sending initial message to server.');
+        info.innerHTML = 'Status: Connected. Press ENTER to blast off!';
+        info.style.backgroundColor = '#49ccd4';
 
         let userPrompt = MYLIBRARY.helloWorld();
 
-        if (terminal.innerHTML == "") {
-            terminal.innerHTML = "> " + userPrompt;
+        if (terminal.innerHTML == '') {
+            terminal.innerHTML = '> ' + userPrompt;
         }
 
         socket.isAlive = true;
 
-        let message = "";
+        let message = '';
         let messages = [];
         let commands = [];
         let commNum = 0;
         let up = 0;
         let down = 0;
         let ctrl = false;
+        let pinged = false;
 
         // Listen for messages
-        socket.onmessage = (event) => {
+        socket.onmessage = event => {
             message = event.data;
 
             var myblob = new Blob([message], {
-                type: 'text/plain'
+                type: 'text/plain',
             });
 
             var reader = new FileReader();
-            reader.addEventListener("loadend", function() {
+            reader.addEventListener('loadend', function() {
                 message = reader.result;
 
-                if (message == "pong") {
+                if (message == 'pong') {
                     heartbeat();
+                    pinged = true;
                     return;
                 }
 
                 if (message.includes('\r')) {
-                    message = message.replace(/\r/g,"");
-                    terminal.innerHTML = terminal.innerHTML.replace(/.*$/, message);
-                }
-
-                else {
+                    message = message.replace(/\r/g, '');
+                    terminal.innerHTML = terminal.innerHTML.replace(
+                        /.*$/,
+                        message
+                    );
+                } else {
                     message = ansi_up.ansi_to_html(message);
                     console.log('message:', message);
                     console.log('typeof message:', typeof message);
@@ -203,55 +209,58 @@ function doTerminal(terminal, socket) {
 
             reader.readAsText(myblob);
 
-            messages = message.split("\n");
-            terminal.scrollTop = terminal.scrollHeight;
+            messages = message.split('\n');
+            if (pinged == false) {
+                terminal.scrollTop = terminal.scrollHeight;
+            }
             zigzagPort(message);
-        }
+        };
 
         // https://stackoverflow.com/questions/22092762/how-to-detect-ctrlc-and-ctrlv-key-pressing-using-regular-expression/22092839
-        terminal.addEventListener("keydown", (e) => {
-            e = e || window.event;
-            var key = e.which || e.keyCode; // keyCode detection
-            var ctrl = e.ctrlKey ? e.ctrlKey : ((key === 17) ? true : false); // ctrl detection
+        terminal.addEventListener(
+            'keydown',
+            e => {
+                e = e || window.event;
+                var key = e.which || e.keyCode; // keyCode detection
+                var ctrl = e.ctrlKey ? e.ctrlKey : key === 17 ? true : false; // ctrl detection
 
-            if ( key == 76 && ctrl ) {
-                console.log("Ctrl + L Pressed !");
-                let lines = terminal.innerHTML.split("\n");
-                terminal.innerHTML = lines[lines.length - 1];
-                event.preventDefault();
-                e.preventDefault();
-                setCaret(terminal);
-            }
-            else if ( key == 67 && ctrl ) {
-                console.log("Ctrl + C Pressed !");
-                socket.send("SIGINT");
-                terminal.innerHTML += "\n";
-            }
+                if (key == 76 && ctrl) {
+                    console.log('Ctrl + L Pressed !');
+                    let lines = terminal.innerHTML.split('\n');
+                    terminal.innerHTML = lines[lines.length - 1];
+                    event.preventDefault();
+                    e.preventDefault();
+                    setCaret(terminal);
+                } else if (key == 67 && ctrl) {
+                    console.log('Ctrl + C Pressed !');
+                    socket.send('SIGINT');
+                    terminal.innerHTML += '\n';
+                } else if (key == 90 && ctrl) {
+                    console.log('Ctrl + Z Pressed !');
+                    socket.send('SIGTSTP');
+                    terminal.innerHTML += '\n';
+                } else if (key == 85 && ctrl) {
+                    console.log('Ctrl + U Pressed !');
+                    e.preventDefault();
+                    terminal.innerHTML = terminal.innerHTML.replace(
+                        /.*$/,
+                        '&gt '
+                    );
+                    setCaret(terminal);
+                }
+            },
+            false
+        );
 
-            else if ( key == 90 && ctrl ) {
-                console.log("Ctrl + Z Pressed !");
-                socket.send("SIGTSTP");
-                terminal.innerHTML += "\n";
-            }
-
-            else if ( key == 85 && ctrl ) {
-                console.log("Ctrl + U Pressed !");
-                e.preventDefault();
-                terminal.innerHTML = terminal.innerHTML.replace(/.*$/ ,"&gt ");
-                setCaret(terminal);
-            }
-
-        },false);
-
-        terminal.onkeydown = (event) => {
+        terminal.onkeydown = event => {
             let key = event.keyCode;
-            let lines = terminal.textContent.split("\n");
+            let lines = terminal.textContent.split('\n');
 
             if (key == 8) {
                 if (lines[lines.length - 1].length <= 1) {
                     event.preventDefault();
                 }
-            }
+            } else if (key == 38) {
 
             /*
             else if (key == 9) {
@@ -261,59 +270,56 @@ function doTerminal(terminal, socket) {
                 socket.send("TAB");
             }
             */
-
-            else if (key == 38) {
                 event.preventDefault();
-                if ((up - down) < commands.length && down <= up) {
+                if (up - down < commands.length && down <= up) {
                     up += 1;
-                    terminal.innerHTML = terminal.innerHTML.replace(/.*$/ ,"> " + commands[commands.length - up + down]);
+                    terminal.innerHTML = terminal.innerHTML.replace(
+                        /.*$/,
+                        '> ' + commands[commands.length - up + down]
+                    );
                 }
-            }
-
-            else if (key == 40) {
+            } else if (key == 40) {
                 event.preventDefault();
-                if (down < up && (up - down) <= commands.length) {
+                if (down < up && up - down <= commands.length) {
                     down += 1;
                     if (down == up) {
-                        terminal.innerHTML = terminal.innerHTML.replace(/.*$/ ,"> ");
-                    }
-                    else {
-                        terminal.innerHTML = terminal.innerHTML.replace(/.*$/ ,"> " + commands[commands.length - up + down]);
+                        terminal.innerHTML = terminal.innerHTML.replace(
+                            /.*$/,
+                            '> '
+                        );
+                    } else {
+                        terminal.innerHTML = terminal.innerHTML.replace(
+                            /.*$/,
+                            '> ' + commands[commands.length - up + down]
+                        );
                     }
                 }
-            }
-
-            else if (key == 13)
-            {
+            } else if (key == 13) {
                 let comm = '';
                 event.preventDefault();
-                terminal.innerHTML += "\n";
+                terminal.innerHTML += '\n';
                 up = 0;
                 down = 0;
-                comm = lines[lines.length-1];
-                comm = comm.replace(/\> /g, "");
-                comm = comm.replace(/^[ ]*/g, "");
-                console.log("you entered:", comm);
+                comm = lines[lines.length - 1];
+                comm = comm.replace(/\> /g, '');
+                comm = comm.replace(/^[ ]*/g, '');
+                console.log('you entered:', comm);
 
-                if (comm == "clear") {
+                if (comm == 'clear') {
                     event.preventDefault();
-                    terminal.innerHTML = "> ";
+                    terminal.innerHTML = '> ';
                     commands[commNum] = comm;
                     commNum += 1;
-                }
-
-                else if (comm == "") {
+                } else if (comm == '') {
                     event.preventDefault();
-                    terminal.innerHTML += "\n> ";
-                }
-
-                else {
+                    terminal.innerHTML += '\n> ';
+                } else {
                     socket.send(comm);
                     commands[commNum] = comm;
                     commNum += 1;
 
-                    if (comm == "zigzag-server") {
-                        let terminals = $(".terminals");
+                    if (comm == 'zigzag-server') {
+                        let terminals = $('.terminals');
                         if (terminals.length < 2) {
                             dup();
                         }
@@ -324,6 +330,6 @@ function doTerminal(terminal, socket) {
             }
 
             setCaret(terminal);
-        }
-    }
+        };
+    };
 }
--- public/theme.js
+++ public/theme.js
@@ -1,36 +1,33 @@
 $(document).ready(function() {
-    let theme = localStorage.getItem("theme");
+    let theme = localStorage.getItem('theme');
 
-    if (theme == "dark") {
+    if (theme == 'dark') {
         setDark();
-    }
-
-    else if (theme == "light") {
+    } else if (theme == 'light') {
         setLight();
     }
 
     try {
-        document.getElementById("darkswitch").onclick = function() {
+        document.getElementById('darkswitch').onclick = function() {
             setDark();
         };
 
-        document.getElementById("lightswitch").onclick = function() {
+        document.getElementById('lightswitch').onclick = function() {
             setLight();
         };
-
-    }
-    catch(err) {
-        console.log("Theme error.");
+    } catch (err) {
+        console.log('Theme error.');
     }
 
     function setDark() {
-        localStorage.setItem("theme", "dark");
-        document.getElementById("dark").href = "/stylesheets/dark.css";
+        localStorage.setItem('theme', 'dark');
+        document.getElementById('dark').href = '/stylesheets/dark.css';
+        document.getElementById('darkswitch').checked = true;
     }
 
     function setLight() {
-        localStorage.setItem("theme", "light");
-        document.getElementById("dark").href = "";
+        localStorage.setItem('theme', 'light');
+        document.getElementById('dark').href = '';
+        document.getElementById('lightswitch').checked = true;
     }
 });
-
